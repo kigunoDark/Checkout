1a5add042e16b7d040bd874b1629746fa982b1af
foxBuka <krushenitskii@gmail.com>
2018-11-09 19:22:26 +0300

    Pssport, bodyparser, and session with env

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 var express = require("express");
+var passport = require("passport");
+var session = require("express-session");
+var bodyParser = require("body-parser");
+var env = require("dotenv").load();
 var app = express();
+
+//For BodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// For Passport
+
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+); // session secret
+
+app.use(passport.initialize());
+
+app.use(passport.session()); // persistent login sessionsvar app = express();
 
 app.get("/", function(req, res) {
   res.send("Welcome to Passport with Sequelize");
 });
 
-app.listen(5000, function(err) {
+app.listen(3000, function(err) {
   if (!err) console.log("Site is live");
   else console.log(err);
 });
